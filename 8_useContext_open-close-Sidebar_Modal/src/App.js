442738f4d0b5fe6b34352b0a1c4a45c7c414f0b4
Thien Vu <vuthienktb@gmail.com>
2021-03-23 16:13:05 +0700

    practice useContext with open close sidebar and modal

---
+++ 8_useContext_open-close-Sidebar_Modal/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Home from './Home';
+import Modal from './Modal';
+import Sidebar from './Sidebar';
+
+
+function App() {
+  return (
+    <section>
+      <Home />  
+      <Modal />
+      <Sidebar />
+    </section>
+  );
+}
+
+export default App;
